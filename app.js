31e3222db8cfca4e34d18a760e9b0adc8455a1cc
Bosspro2728 <bosspro2728@gmail.com>
2024-05-14 22:20:28 +0200

    fix particle speed

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 10,
+        "speed": 20,
         "direction": "none",
         "random": false,
         "straight": false,
